738e57b04153af41039473f77f62c1ebf3f91f58
zackattacknyu <zackattacknyu@gmail.com>
2016-12-08 15:40:12 -0800

    df

--- toSend/birthDeathYellowstone.m
+++ toSend/birthDeathYellowstone.m
@@ -1,3 +1,7 @@
+%{
+This is the yellowstone wolf data
+%}
+
 wolfNumbers = [21	38	62	82	71	118	130	148 ...
     174	172	118	138 170	125	95	98	99	82 95 102];
 
@@ -6,6 +10,11 @@ numPupsBorn = [0	15	65	38	40	69	44	64	74 ...
 
 numPupsSurvived = [0	10	42	32	22	60	43	48 ...
     59	59	22	60	64	22	23	38	34	20	41	40];
+
+%{
+Here is estimate r from 1995-2003.
+    I then make its graph
+%}
 
 tVals = (0:8)';
 wolvesUntil2003 = (wolfNumbers(1:9))';
@@ -16,9 +25,14 @@ rVal = ff.b;
 xxPlot = (0:0.01:8)';
 yyPlot = n0.*exp(rVal.*xxPlot);
 
+%{
+Here I estimate the birth rate
+starting from 1996, since they were reintroduced
+the year before.
 
-%estimate the birth rate
-%model this starting at 1996, since they were reintroduced in 1995
+I then make the birth rate graph values
+%}
+
 totalWolvesIfNoDeath = cumsum(numPupsSurvived)+wolfNumbers(1);
 tValsNew = (0:7)';
 totalWolvesUse = totalWolvesIfNoDeath(1:8)';
@@ -29,7 +43,7 @@ rValBirth = birthData.b;
 xxPlot2 = (0:0.01:7)';
 yyPlot2 = n0birth.*exp(rValBirth.*xxPlot2);
 
-%%
+%make the population growth graph
 figure
 subplot(1,2,1)
 hold on
@@ -42,6 +56,7 @@ ylabel('Number of Wolves');
 title('Number of Wolves in Yellowstone 1995-2003');
 hold off
 
+%make the birth rate estimate graph
 subplot(1,2,2)
 hold on
 plot(tValsNew,totalWolvesUse,'bx','LineWidth',2);
