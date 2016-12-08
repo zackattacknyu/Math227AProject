78aba117084961470a74a4dd1d2a7b292f7f2878
zackattacknyu <zackattacknyu@gmail.com>
2016-12-08 15:12:52 -0800

    fg

---
+++ toSend/stochFitPOC.m
@@ -0,0 +1,18 @@
+%This is the Proof of Concept for the stochastic modelling code
+% it re-creates the Figure 5.9 from Section 5.6.2 of the de Vries book 
+
+birthRate = 0.333;
+deathRate = 0.3;
+
+numSteps = 200;
+numTraj = 50; 
+
+initNum = 10.*ones(numTraj,1);
+
+[times,numWolves] = runStochBirthDeath(birthRate,deathRate,initNum,numSteps,numTraj);
+
+figure
+stairs(times',numWolves')
+xlabel('t');
+ylabel('N');
+axis([0 9 0 inf]);
